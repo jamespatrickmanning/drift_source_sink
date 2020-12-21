1ebccf0c7a6dc1b7d76785707f8b6e65369d5259
Jim Manning <james.manning@noaa.gov>
2020-12-21 11:16:49 -0500

    Add files via upload

--- drift_sink.py
+++ drift_sink.py
@@ -2,12 +2,10 @@
 """
 Created on Wed Aug 27 10:14:02 2014
 
-@author: hxu
-this extracts drifter data based on time range, geographic range adn/or drifter id, then plots them.
-the first point of every drifter should be in this range.
+this extracts drifter data based on time range, geographic range and/or drifter id, then plots them.
 The ouput html file will be named by 'sink_<region>.html' in same folder as this program. 
 input values: time period,gbox(maxlon, minlon,maxlat,minlat),or time period and id
-function uses: getobs_drift_byrange,getobs_drift_byidrange,colors,getobs_drift_byid,point_in_poly
+function used: getobs_drift_byrange,getobs_drift_byidrange,colors,getobs_drift_byid,point_in_poly
 output : a plot html file to show drifter track on google map.
 
 Modifications by JiM in March 2019 to:
@@ -15,7 +13,7 @@ Modifications by JiM in March 2019 to:
   - deliniate by month and length of track
 Note that we still need to run a sed command to add key after running this
 """
-#import basemap_xu
+
 import datetime as dt
 import sys
 import os
@@ -26,20 +24,30 @@ from drifter_functions import getobs_drift_byrange,getobs_drift_byid,point_in_po
 ops=os.defpath
 pydir='../'
 sys.path.append(pydir)
+
 #################Input values#############################################
 # HARDCODES ##########
-input_time=[dt.datetime(1980,1,1,0,0,0,0,pytz.UTC),dt.datetime(2018,10,15,0,0,0,0,pytz.UTC)] # start time and end time
+#case='sink_liz_gsc' # name the case study with PI and region
+case='sink_cadrin_nshore'
+input_time=[dt.datetime(1980,1,1,0,0,0,0,pytz.UTC),dt.datetime(2020,10,15,0,0,0,0,pytz.UTC)] # start time and end time
 #gbox=[-70.035594,-70.597883,42.766619,42.093197] #  maxlon, minlon,maxlat,minlat for Stellwagen
-gbox=[-69.33,-69.75,41.5,41.] #  maxlon, minlon,maxlat,minlat for Nant shoals for Cap Brady
-months=[8,9] # months of interest
-days=dt.timedelta(days=6*7) # maximum length of track to plot where, for example, 6*7 is six weeks
+#gbox=[-69.33,-69.75,41.5,41.] #  maxlon, minlon,maxlat,minlat for Nant shoals for Cap Brady
+#gbox=[-66.,-67.,42.0,41.5] # Liz Brokks NEP
+#gbox=[-68.5,-69.3833,41.5, 40.75] # Liz Brooks GSC square case
+gbox=[-69.5,-71.,43.,41.5] #north shore
+#gbox=[-69.3833, -68.5, -68.5, -68.75,41.5, 41.5, 40.75, 40.75] #where she actually wanted polygon longitude:  
+months=[1,2,3,4,5,6,7,8,9,10,11,12] # months of interest
+numweeks=6 # number of weeks to track
 #↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑Input values↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑#
-polygon=[(gbox[0],gbox[2]),(gbox[0],gbox[3]),(gbox[1],gbox[3]),(gbox[1],gbox[2])] #set polygon   
+
+days=dt.timedelta(days=numweeks*7) # maximum number of day to plot where, for example, 6*7 is six weeks
+polygon=[(gbox[0],gbox[2]),(gbox[0],gbox[3]),(gbox[1],gbox[3]),(gbox[1],gbox[2])] #set polygon in case of a square
+#polygon=[(gbox[0],gbox[4]),(gbox[1],gbox[5]),(gbox[3],gbox[6]),(gbox[4],gbox[7])  
 time,ids,lats,lons=getobs_drift_byrange(gbox,input_time)
 #mymap = basemap_xu.maps(np.mean(lats), np.mean(lons), 12)  #set center point of the map
 id=list(set(ids))
 colors=hexcolors(len(id))  #get hex colors,like '00FF00'
-gmap3 = gmplot.GoogleMapPlotter(np.mean([gbox[2],gbox[3]]),np.mean([gbox[0],gbox[1]]), 9)
+gmap3 = gmplot.GoogleMapPlotter(np.mean([gbox[2],gbox[3]]),np.mean([gbox[0],gbox[1]]), 7)
 for k in range(len(id)):
       time,ids,lat,lon=getobs_drift_byid(id[k],input_time)  # get data by id
       for z in range(len(lat)-1):  # make plotting drifter start in gbox
@@ -55,13 +63,17 @@ for k in range(len(id)):
                gmap3.plot(lat,lon,colors[k], edge_width = 2.5) 
                break
 ranges=[(gbox[2],gbox[0]),(gbox[3],gbox[0]),(gbox[3],gbox[1]),(gbox[2],gbox[1]),(gbox[2],gbox[0])] #plot range you gave
-gmap3.plot(ranges[0],ranges[1])
+#gmap3.plot(ranges[0],ranges[1],color[0],edgewidth=10)
+#gmap3.plot([gbox[2],gbox[2],gbox[3],gbox[3],gbox[2]],[gbox[0],gbox[1],gbox[1],gbox[0],gbox[0]],colors[0],edgewidth=10)
+#gmap3.plot([gbox[4],gbox[5],gbox[6],gbox[7],gbox[4]],[gbox[0],gbox[1],gbox[2],gbox[3],gbox[0]],colors[0],edgewidth=10)
+
 #mymap.addpath(ranges,'red')#00FF00    
 #mymap.draw('./sink_through'+dt.datetime.now().strftime('%Y-%m-%d %H:%M')+'.html')
 gmap3.draw('/net/pubweb_html/drifter/test.html')
 
+
 #gmap does not have a apikey function, so lets substitute it.
-#sed '/<\/body>/i <script async defer src="https:\/\/maps.googleapis.com\/maps\/api\/js?v=3&client=gme-noaa&channel=NMFS.NEFSC.OCB.DRIFTERS&callback=initMap"><\/script>' test.html > sink_stellwagen.html 
+#os.system(sed '/<\/body>/i <script async defer src="https:\/\/maps.googleapis.com\/maps\/api\/js?v=3&client=gme-noaa&channel=NMFS.NEFSC.OCB.DRIFTERS&callback=initMap"><\/script>' test.html > sink_pi_region.html 
 '''
 tempHolder=''
 oldLine='</body>'
